19169a4ae4873411aafa7d6f3986270fc69ad75b
Art Lasovsky <artlasovsky@gmail.com>
2020-03-13 15:39:51 +0200

    initial electron starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test from './components/Test'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <Test />
         <a
           className="App-link"
           href="https://reactjs.org"
